cc05d764eca71e8650b998c32f8f760e780c1368
shixiang <shixiangcool@163.com>
2018-06-01 23:35:11 +0800

    完成播放页面的歌词滚动

--- src/js/song/song.js
+++ src/js/song/song.js
@@ -7,12 +7,50 @@
     render(data={}) {
       let {song} = data
       this.$el.find('audio').attr('src', song.songUrl)
+      this.$el.find('.bg-img').css('background-image', `url(${song.coverUrl})`)
+      this.$el.find('.cover').attr('src', song.coverUrl)
+      this.$el.find('.song-desc>h1').text(song.name)
+
+      song.lyric.split('\n').map((val) => {
+        let pTag = document.createElement('p')
+        let reg = /\[([\d:.]+)\](.+)/
+        let matches = val.match(reg)
+        if (matches) {
+          pTag.textContent = matches[2]
+          let timeArr = matches[1].split(':')
+          let newTime = timeArr[0] * 60 + parseFloat(timeArr[1])
+          pTag.setAttribute('data-time', newTime)
+        } else {
+          pTag.textContent = val
+        }
+        this.$el.find('.lyric-wrapper>.lyric').append(pTag)
+      })
     },
     audioPlay() {
       this.$el.find('audio')[0].play()
     },
     audioPause() {
       this.$el.find('audio')[0].pause()
+    },
+    showLyric(time) {
+      let allPTag = this.$el.find('.lyric-wrapper>.lyric>p')
+      let myPTag
+      for (let i = 0; i < allPTag.length; i++) {
+        if (i === allPTag.length - 1) {
+          myPTag = allPTag[i]
+          break
+        } else {
+          let previousTime = allPTag.eq(i).attr('data-time')
+          let nextTime = allPTag.eq(i+1).attr('data-time')
+          if (previousTime <= time && time < nextTime) {
+            myPTag = allPTag[i]
+            break
+          }          
+        }
+      }
+      let sildeLength = myPTag.getBoundingClientRect().top - this.$el.find('.lyric-wrapper>.lyric')[0].getBoundingClientRect().top
+      this.$el.find('.lyric-wrapper>.lyric').css({transform: `translateY(${-(sildeLength - 25)}px)`})
+      $(myPTag).addClass('active').siblings('.active').removeClass('active')
     }
   }
   let model = {
@@ -76,6 +114,14 @@
           $(e.currentTarget).find('div').removeClass('active')
           $(e.currentTarget).siblings('.cover').removeClass('paused')          
         }
+      })
+      this.view.$el.find('audio').on('ended', () => {
+        this.view.$el.find('.cover').addClass('paused')
+      })
+
+      let audioTag = document.querySelector('audio')
+      this.view.$el.find('audio').on('timeupdate', () => {
+        this.view.showLyric(audioTag.currentTime)
       })
     }
   }
